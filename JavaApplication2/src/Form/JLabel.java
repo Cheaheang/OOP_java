a150270a62172db962e13c66d0e5940199779832
Cheaheang <101448464+Cheaheang@users.noreply.github.com>
2023-03-09 11:10:13 +0700

    Add files via upload

---
+++ JavaApplication2/src/Form/JLabel.java
@@ -0,0 +1,16 @@
+package Form;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JLabel {
+    public void setBackground(Color color) {
+    }
+
+    public void setIcon(ImageIcon imageIcon) {
+    }
+
+    public void setText(String jLabel2) {
+    }
+}
+
